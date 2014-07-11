3e8ef55c6aa3c3be797e964327f9f311b11bafed
lost <lost@com>
2014-07-12 02:06:38 +0800

    add cernet20

---
+++ cernet20/config.js
@@ -0,0 +1,16 @@
+var cernet20_config = {
+    // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+    transition: 6,
+
+    //切换间隔时间，毫秒
+    slide_interval: 5000,
+
+    //页面配置
+    pages: [{
+        title: 'HTTPS 测量',
+        url: 'http://www.bing.com'
+    }, {
+        title: 'Passive DNS 监控',
+        url: 'http://www.baidu.com'
+    }]
+};
